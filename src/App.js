f651b7bd67e3de74e453b709c740e4934846c115
Himanshu Malhotra <himanshumalhotra@Himanshus-Mac-mini.local>
2025-10-23 22:17:24 +0530

    Added Base routing in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/home';
+import Auth from './pages/auth/auth';
+import {
+  createBrowserRouter,
+
+  RouterProvider,
+} from "react-router-dom";
+
+
+const appRoutes = createBrowserRouter([
+  {
+    path : "/" ,
+    element : <Home />    
+  } , 
+  {
+    path : "/auth",
+    element : <Auth />
+  }
+])
+
 function App() {
   return (
     <div className="App">
-      <Home />
+     <RouterProvider router={appRoutes} />
     </div>
   );
 }
---
+++ src/pages/auth/auth.jsx
@@ -0,0 +1,8 @@
+function Auth(){
+    return(
+        <h1>Auth Page</h1>
+    )
+}
+
+
+export default Auth
